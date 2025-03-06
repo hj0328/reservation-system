e39b76857e29478d01320c5ab61ce2a96325a80a
hojun <hjlee9303@gmail.com>
2025-03-06 13:52:55 +0900

    [Refactor] Product의 불필요 코드 리팩토링

--- src/main/java/kr/or/connect/reservation/domain/product/dto/ProductSeatScheduleRequest.java
+++ src/main/java/kr/or/connect/reservation/domain/product/dto/ProductSeatScheduleRequest.java
@@ -1,6 +1,5 @@
 package kr.or.connect.reservation.domain.product.dto;
 
-import lombok.Builder;
 import lombok.Getter;
 
 import javax.validation.constraints.NotNull;
@@ -16,11 +15,4 @@ public class ProductSeatScheduleRequest {
     private LocalDateTime eventDateTime;
     @NotNull
     private String seatType;
-
-    @Builder
-    public ProductSeatScheduleRequest(LocalDateTime eventDateTime, Long placeId, String seatType) {
-        this.placeId = placeId;
-        this.eventDateTime = eventDateTime;
-        this.seatType = seatType;
-    }
 }
