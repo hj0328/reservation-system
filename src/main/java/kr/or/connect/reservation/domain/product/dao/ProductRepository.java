56ec262a667c8fe199a6846d0550b6cea6080876
hojun <hjlee9303@gmail.com>
2025-04-25 00:06:08 +0900

    [Test] offset pagination 다시 테스트

--- src/main/java/kr/or/connect/reservation/domain/product/dao/ProductRepository.java
+++ src/main/java/kr/or/connect/reservation/domain/product/dao/ProductRepository.java
@@ -20,6 +20,13 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
             "LIMIT :pageSize ", nativeQuery = true)
     List<Product> findByTitleStartsWith(String title, Long productId, Integer pageSize);
 
+    @Query(value = "SELECT product0_.product_id, product0_.created_at, product0_.updated_at , product0_.category_id, product0_.description, product0_.release_date , product0_.running_time , product0_.title  " +
+            "FROM product product0_  " +
+            "WHERE product0_.title LIKE CONCAT(:title, '%') " +
+            "ORDER BY product0_.product_id, product0_.title " +
+            "LIMIT :pageSize OFFSET :offset ", nativeQuery = true)
+    List<Product> findByTitleStartsWithTemp(String title, Integer offset, Integer pageSize);
+
     @Query(value = "SELECT product0_.product_id , product0_.created_at, product0_.updated_at, product0_.category_id, product0_.description, product0_.release_date , product0_.running_time , product0_.title  " +
             "FROM product product0_ left outer join category category1_ " +
                 "on product0_.category_id=category1_.category_id  " +
@@ -28,6 +35,21 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
             "LIMIT :pageSize ", nativeQuery = true)
     List<Product> findByTitleStartingWithAndCategoryId(String title, Long categoryId, Long productId, Integer pageSize);
 
+    @Query(value = "SELECT product0_.product_id , product0_.created_at, product0_.updated_at, product0_.category_id, product0_.description, product0_.release_date , product0_.running_time , product0_.title  " +
+            "FROM product product0_ left outer join category category1_ " +
+            "on product0_.category_id=category1_.category_id  " +
+            "WHERE product0_.title LIKE CONCAT(:title, '%') AND category1_.category_id=:categoryId " +
+            "ORDER BY product0_.product_id, product0_.title " +
+            "LIMIT :pageSize OFFSET :offset ", nativeQuery = true)
+    List<Product> findByTitleStartingWithAndCategoryIdTemp(String title, Long categoryId, Integer offset, Integer pageSize);
+
+
     @Query("SELECT p FROM Product p WHERE p.id > :productId ORDER BY p.id, p.releaseDate Desc, p.title")
     List<Product> findAllProducts(Long productId, PageRequest pageRequest);
+
+    @Query("SELECT p FROM Product p ORDER BY p.id, p.releaseDate Desc, p.title")
+    List<Product> findAllProductsTemp(Long productId, PageRequest pageRequest);
+
+    @Query("SELECT p FROM Product p WHERE p.category.id = :categoryId ORDER BY p.id, p.releaseDate Desc, p.title")
+    List<Product> findAllByCategoryIdTemp(Long categoryId, PageRequest pageRequest);
 }
