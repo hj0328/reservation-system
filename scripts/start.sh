1fa6c0d3edd644a9f0e4e7eafae54c352cb8b734
hojun <hjlee9303@gmail.com>
2025-04-25 22:32:24 +0900

    [CI/CD] 힙 사이즈 설정 - 초기 힙 512mb - 최대 힙 1GB

--- scripts/start.sh
+++ scripts/start.sh
@@ -27,7 +27,7 @@ IDLE_PROFILE=$(find_idle_profile)
 
 echo "> $JAR_NAME 를 profile=$IDLE_PROFILE 로 실행합니다."
 
-nohup java -jar \
+nohup java -Xms512m -Xmx1024m -jar \
   -Dspring.config.location=classpath:/application.properties,classpath:/application-$IDLE_PROFILE.properties,\
 file:/home/ec2-user/app/application-real-db.properties \
   -Dspring.profiles.active=$IDLE_PROFILE \
