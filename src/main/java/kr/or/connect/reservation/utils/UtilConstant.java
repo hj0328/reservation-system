b55432c5f95c757ae9f233882ae54eaf0dbe790f
hojun <hjlee9303@gmail.com>
2025-05-02 20:39:29 +0900

    [Feat] page size 기존으로 변경

--- src/main/java/kr/or/connect/reservation/utils/UtilConstant.java
+++ src/main/java/kr/or/connect/reservation/utils/UtilConstant.java
@@ -5,8 +5,8 @@ public class UtilConstant {
     public static final String MEMBER_ID = "memberId";
     public static final String USER_EMAIL = "userEmail";
     public static final Long ALL_PRODUCTS = 0L;
-    public static final Integer PRODUCT_PAGE_SIZE = 100;
-    public static final Integer RESERVATION_PAGE_SIZE = 100;
+    public static final Integer PRODUCT_PAGE_SIZE = 20;
+    public static final Integer RESERVATION_PAGE_SIZE = 20;
 
     public static final String RESERVATION_INFO_ID = "reservationInfoId";
 
